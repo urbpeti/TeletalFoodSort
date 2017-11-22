7b2501262ad8d4985a4fb02664f366f7eca17c38
Urbanovics Péter <urbanovics.peter@ArgonSoft.hu>
2017-11-22 09:07:35 +0100

    cheerio

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 const axios = require('axios');
-const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
+const cheerio = require('cheerio');
 
 axios.get('https://www.teletal.hu/etlap').then((response)=>{
-	const html = new JSDOM(response.data);
-	console.log(html.window.document.querySelector(".ar")[1]);
+	const dom = cheerio.load(response.data);
+	console.log(dom(".ar").length);
 })
 .catch((error)=>{
 		console.log(error);
